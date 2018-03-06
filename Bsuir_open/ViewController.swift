87081a431657a65629dbc25728625d859f7ba2d9
Andrew Rolya <a.rolya@softteco.com>
2018-03-06 17:41:03 +0300

    Here is my commit message

---
+++ Bsuir_open/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bsuir_open
+//
+//  Created by Andrew Rolya on 3/6/18.
+//  Copyright © 2018 Andrew Rolya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
